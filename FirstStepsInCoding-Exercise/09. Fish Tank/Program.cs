3f1209a3283abdbd7cc54f58579686297f1b5213
GStaykov98 <165571267+GStaykov98@users.noreply.github.com>
2025-01-29 16:25:06 +0200

    Add files via upload

---
+++ FirstStepsInCoding-Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,10 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double occupationPercent = double.Parse(Console.ReadLine());
+
+int fishTankArea = length * width * height;
+double areaInLitres = fishTankArea / 1000.0;
+double neededLitres = areaInLitres - (areaInLitres * (occupationPercent / 100));
+
+Console.WriteLine(neededLitres);
