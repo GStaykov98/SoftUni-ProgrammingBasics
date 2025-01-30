efdcec4acff5d330fb129e0d3cf1b1278ffafa52
GStaykov98 <g.staykov98@gmail.com>
2025-01-30 18:16:25 +0200

    Add files via upload

---
+++ Conditional Statements - Exercise/02. Bonus Score/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = int.Parse(Console.ReadLine());
+double bonusPoints = 0;
+
+if (a <= 100)
+{
+    bonusPoints += 5;
+}
+else if (a <= 1000)
+{
+    bonusPoints = a * 0.2;
+}
+else
+{
+    bonusPoints = a * 0.1;
+}
+
+if (a % 2 == 0)
+{
+    bonusPoints += 1;
+}
+
+if (a % 10 == 5)
+{
+    bonusPoints += 2;
+}
+
+Console.WriteLine(bonusPoints);
+Console.WriteLine(a + bonusPoints);
---
+++ Conditional Statements - Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,17 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+minutes += 15;
+
+if (minutes >= 60)
+{
+    minutes = minutes % 60;
+    hours += 1;
+}
+
+if (hours >= 24)
+{
+    hours = hours % 24;
+}
+
+Console.WriteLine($"{hours}:{minutes:d2}");
