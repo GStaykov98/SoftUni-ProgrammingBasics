f2b1561bea7c376358e707dd5bfd368cdcc57dc5
GStaykov98 <g.staykov98@gmail.com>
2025-01-30 18:45:15 +0200

    Add files via upload

---
+++ Conditional Statements - Exercise/04. Toy Shop/Program.cs
@@ -0,0 +1,39 @@
+﻿const double puzzlePrice = 2.6;
+const double dollPrice = 3;
+const double tedyBearPrice = 4.1;
+const double minionPrice = 8.2;
+const double carPrice = 2;
+
+double vacationPrice = double.Parse(Console.ReadLine());
+int puzzleCount = int.Parse(Console.ReadLine());
+int dollCount = int.Parse(Console.ReadLine());
+int tedyBearCount = int.Parse(Console.ReadLine());
+int minionCount = int.Parse(Console.ReadLine());
+int carCount = int.Parse(Console.ReadLine());
+int toysCount = puzzleCount + dollCount + tedyBearCount + minionCount + carCount;
+
+double puzzlesTotalPrice = puzzlePrice * puzzleCount;
+double dollsTotalPrice = dollPrice * dollCount;
+double tedyBearsTotalPrice = tedyBearPrice * tedyBearCount;
+double minionsTotalPrice = minionPrice * minionCount;
+double carsTotalPrice = carPrice * carCount;
+
+double totalPrice = puzzlesTotalPrice + dollsTotalPrice + tedyBearsTotalPrice + minionsTotalPrice + carsTotalPrice;
+
+if (toysCount >= 50)
+{
+    totalPrice -= totalPrice * 0.25;
+}
+
+totalPrice -= totalPrice * 0.1;
+
+if (totalPrice >= vacationPrice)
+{
+    double moneyLeft = totalPrice - vacationPrice;
+    Console.WriteLine($"Yes! {moneyLeft:f2} lv left.");
+}
+else
+{
+    double moneyNeeded = vacationPrice - totalPrice;
+    Console.WriteLine($"Not enough money! {moneyNeeded:f2} lv needed.");
+}
---
+++ Conditional Statements - Exercise/06. World Swimming Record/Program.cs
@@ -0,0 +1,18 @@
+﻿double record = double.Parse(Console.ReadLine());
+double length = double.Parse(Console.ReadLine());
+double secondPerMeter = double.Parse(Console.ReadLine());
+
+double swimmingTime = length * secondPerMeter;
+double delay = Math.Floor(length / 15) * 12.5;
+
+swimmingTime += delay;
+
+if (swimmingTime < record)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {swimmingTime:f2} seconds.");
+}
+else
+{
+    double secondsLeft = swimmingTime - record;
+    Console.WriteLine($"No, he failed! He was {secondsLeft:f2} seconds slower.");
+}
