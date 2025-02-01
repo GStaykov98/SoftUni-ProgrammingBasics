4c3b624eff3834375b07d2a6c2c5abaa2376ec5a
GStaykov98 <g.staykov98@gmail.com>
2025-02-01 17:54:35 +0200

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/11. Fruit Shop/Program.cs
@@ -0,0 +1,124 @@
+﻿string fruit = Console.ReadLine();
+string day = Console.ReadLine();
+double count = double.Parse(Console.ReadLine());
+
+string typeOfDay = null;
+
+if (day == "Monday"||
+	day == "Tuesday"||
+	day == "Wednesday"||
+	day == "Thursday"||
+	day == "Friday")
+{
+	typeOfDay = "Weekday";
+}
+else if (day == "Saturday"||
+		 day == "Sunday")
+{
+	typeOfDay = "Weekend";
+}
+
+switch (fruit)
+{
+	case "banana":
+		if (typeOfDay == "Weekday")
+		{
+			Console.WriteLine($"{2.5 * count:f2}");
+		}
+		else if (typeOfDay == "Weekend")
+		{
+            Console.WriteLine($"{2.7 * count:f2}");
+        }
+		else
+		{
+            Console.WriteLine("error");
+        }
+		break;
+	case "apple":
+        if (typeOfDay == "Weekday")
+        {
+            Console.WriteLine($"{1.2 * count:f2}");
+        }
+        else if (typeOfDay == "Weekend")
+        {
+            Console.WriteLine($"{1.25 * count:f2}");
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+        break;
+    case "orange":
+        if (typeOfDay == "Weekday")
+        {
+            Console.WriteLine($"{0.85 * count:f2}");
+        }
+        else if (typeOfDay == "Weekend")
+        {
+            Console.WriteLine($"{0.9 * count:f2}");
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+        break;
+    case "grapefruit":
+        if (typeOfDay == "Weekday")
+        {
+            Console.WriteLine($"{1.45 * count:f2}");
+        }
+        else if (typeOfDay == "Weekend")
+        {
+            Console.WriteLine($"{1.6 * count:f2}");
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+        break;
+    case "kiwi":
+        if (typeOfDay == "Weekday")
+        {
+            Console.WriteLine($"{2.7 * count:f2}");
+        }
+        else if (typeOfDay == "Weekend")
+        {
+            Console.WriteLine($"{3 * count:f2}");
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+        break;
+    case "pineapple":
+        if (typeOfDay == "Weekday")
+        {
+            Console.WriteLine($"{5.5 * count:f2}");
+        }
+        else if (typeOfDay == "Weekend")
+        {
+            Console.WriteLine($"{5.6 * count:f2}");
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+        break;
+    case "grapes":
+        if (typeOfDay == "Weekday")
+        {
+            Console.WriteLine($"{3.85 * count:f2}");
+        }
+        else if (typeOfDay == "Weekend")
+        {
+            Console.WriteLine($"{4.2 * count:f2}");
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+        break;
+    default:
+        Console.WriteLine("error");
+        break;
+}
---
+++ Conditional Statements Advanced - Lab/12. Trade Commissions/Program.cs
@@ -0,0 +1,84 @@
+﻿string city = Console.ReadLine();
+double sales = double.Parse(Console.ReadLine());
+
+if (sales >= 0 &&
+    sales <= 500)
+{
+	if (city == "Sofia")
+	{
+        Console.WriteLine($"{sales * 0.05:f2}");
+    }
+	else if (city == "Varna")
+	{
+        Console.WriteLine($"{sales * 0.045:f2}");
+    }
+	else if (city == "Plovdiv")
+	{
+        Console.WriteLine($"{sales * 0.055:f2}");
+    }
+	else
+	{
+        Console.WriteLine("error");
+    }
+}
+else if (sales <= 1000)
+{
+    if (city == "Sofia")
+    {
+        Console.WriteLine($"{sales * 0.07:f2}");
+    }
+    else if (city == "Varna")
+    {
+        Console.WriteLine($"{sales * 0.075:f2}");
+    }
+    else if (city == "Plovdiv")
+    {
+        Console.WriteLine($"{sales * 0.08:f2}");
+    }
+    else
+    {
+        Console.WriteLine("error");
+    }
+}
+else if (sales <= 10000)
+{
+    if (city == "Sofia")
+    {
+        Console.WriteLine($"{sales * 0.08:f2}");
+    }
+    else if (city == "Varna")
+    {
+        Console.WriteLine($"{sales * 0.1:f2}");
+    }
+    else if (city == "Plovdiv")
+    {
+        Console.WriteLine($"{sales * 0.12:f2}");
+    }
+    else
+    {
+        Console.WriteLine("error");
+    }
+}
+else if (sales > 10000)
+{
+    if (city == "Sofia")
+    {
+        Console.WriteLine($"{sales * 0.12:f2}");
+    }
+    else if (city == "Varna")
+    {
+        Console.WriteLine($"{sales * 0.13:f2}");
+    }
+    else if (city == "Plovdiv")
+    {
+        Console.WriteLine($"{sales * 0.145:f2}");
+    }
+    else
+    {
+        Console.WriteLine("error");
+    }
+}
+else
+{
+    Console.WriteLine("error");
+}
