1aa5a08099b70daf26d2e63a489ecb0291a4364d
GStaykov98 <g.staykov98@gmail.com>
2025-02-02 23:50:44 +0200

    Add files via upload

---
+++ Conditional Statements Advanced - Exercise/04. Fishing Boat/Program.cs
@@ -0,0 +1,46 @@
+﻿int budget = int.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+int countOfFishers = int.Parse(Console.ReadLine());
+double rent = 0.0;
+
+if (season == "Spring")
+{
+    rent = 3000;
+}
+else if (season == "Summer" ||  season == "Autumn")
+{
+    rent = 4200;
+}
+else if (season == "Winter")
+{
+    rent = 2600;
+}
+
+if (countOfFishers <= 6)
+{
+    rent -= rent * 0.1;
+}
+else if (countOfFishers <= 11)
+{
+    rent -= rent * 0.15;
+}
+else if (countOfFishers >= 12)
+{
+    rent -= rent * 0.25;
+}
+
+if (countOfFishers % 2 == 0 && !(season == "Autumn"))
+{
+    rent -= rent * 0.05;
+}
+
+if (budget - rent >= 0)
+{
+    double moneyLeft = budget - rent;
+    Console.WriteLine($"Yes! You have {moneyLeft:f2} leva left.");
+}
+else
+{
+    double moneyNeeded = rent - budget;
+    Console.WriteLine($"Not enough money! You need {moneyNeeded:f2} leva.");
+}
---
+++ Conditional Statements Advanced - Exercise/05. Journey/Program.cs
@@ -0,0 +1,45 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+string destination = string.Empty;
+string typeOfBreak = string.Empty;
+double moneySpent = 0;
+
+if (budget <= 100)
+{
+	destination = "Bulgaria";
+
+	if (season == "summer")
+	{
+		moneySpent = budget * 0.3;
+		typeOfBreak = "Camp";
+	}
+	else if (season == "winter")
+	{
+		moneySpent = budget * 0.7;
+		typeOfBreak = "Hotel";
+	}
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+
+    if (season == "summer")
+    {
+        moneySpent = budget * 0.4;
+        typeOfBreak = "Camp";
+    }
+    else if (season == "winter")
+    {
+        moneySpent = budget * 0.8;
+        typeOfBreak = "Hotel";
+    }
+}
+else if (budget > 1000)
+{
+    destination = "Europe";
+    moneySpent = budget * 0.9;
+    typeOfBreak = "Hotel";
+}
+
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{typeOfBreak} - {moneySpent:f2}");
