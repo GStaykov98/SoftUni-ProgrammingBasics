0c8d2551bc026ce41b18eb63a6086a0d07d75e5b
GStaykov98 <g.staykov98@gmail.com>
2025-02-07 12:06:41 +0200

    Add files via upload

---
+++ Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,16 @@
+﻿string input;
+
+while ((input = Console.ReadLine()) != "End")
+{
+    string destination = input;
+    double budget = double.Parse(Console.ReadLine());
+    double moneySaved = 0;
+
+    while (budget > moneySaved)
+    {
+        double currentMoney = double.Parse(Console.ReadLine());
+        moneySaved += currentMoney;
+    }
+
+    Console.WriteLine($"Going to {destination}!");
+}
