5078ae41572df45d4c438bce8e1927f50f14a4d9
GStaykov98 <g.staykov98@gmail.com>
2025-02-07 11:40:24 +0200

    Add files via upload

---
+++ While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int pieces = a * b;
+string input;
+
+while ((input = Console.ReadLine()) != "STOP")
+{
+    int piecesToTake = int.Parse(input);
+
+    pieces -= piecesToTake;
+
+    if (pieces < 0)
+    {
+        Console.WriteLine($"No more cake left! You need {Math.Abs(pieces)} pieces more.");
+        return;
+    }
+}
+
+Console.WriteLine($"{pieces} pieces are left.");
---
+++ While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int freeSpace = a * b * c;
+string input;
+
+while ((input = Console.ReadLine()) != "Done")
+{
+    int boxes = int.Parse(input);
+    freeSpace -= boxes;
+
+    if (freeSpace < 0)
+    {
+        Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace)} Cubic meters more.");
+        return;
+    }
+}
+
+Console.WriteLine($"{freeSpace} Cubic meters left.");
