011793cb295b7dc3c8ae7a8e0448a54a0773f4eb
GStaykov98 <g.staykov98@gmail.com>
2025-02-07 11:48:03 +0200

    Add files via upload

---
+++ Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+int countOfCombinations = 0;
+
+for (int i = 0; i <= n; i++)
+{
+    for (int j = 0; j <= n; j++)
+    {
+        for (int k = 0; k <= n; k++)
+        {
+            if (i + j + k == n)
+            {
+                countOfCombinations++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(countOfCombinations);
