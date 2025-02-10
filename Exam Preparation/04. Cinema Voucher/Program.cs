23dab975144c6275b6dcaccaa7ddb78dece8abe8
GStaykov98 <g.staykov98@gmail.com>
2025-02-10 16:38:55 +0200

    Add files via upload

---
+++ Exam Preparation/04. Cinema Voucher/Program.cs
@@ -0,0 +1,35 @@
+﻿int budget = int.Parse(Console.ReadLine());
+string input;
+int movieTickets = 0;
+int productsBought = 0;
+
+while ((input = Console.ReadLine()) != "End")
+{
+	if (input.Length > 8)
+	{
+		int price = (int)input[0] + input[1];
+		budget -= price;
+
+		if (budget < 0)
+		{
+			break;
+		}
+
+		movieTickets++;
+	}
+	else
+	{
+		int price = (int)input[0];
+		budget -= price;
+
+        if (budget < 0)
+        {
+            break;
+        }
+
+        productsBought++;
+	}
+}
+
+Console.WriteLine(movieTickets);
+Console.WriteLine(productsBought);
---
+++ Exam Preparation/05. Movie Ratings/Program.cs
@@ -0,0 +1,30 @@
+﻿int moviesCount = int.Parse(Console.ReadLine());
+string highestRatingMovie = string.Empty;
+double highestRating = double.MinValue;
+string lowestRatingMovie = string.Empty;
+double lowestRating = double.MaxValue;
+double totalRating = 0;
+
+
+for (int i = 0; i < moviesCount; i++)
+{
+    string currentMovie = Console.ReadLine();
+    double currentRating = double.Parse(Console.ReadLine());
+    totalRating += currentRating;
+
+    if (currentRating > highestRating)
+    {
+        highestRating = currentRating;
+        highestRatingMovie = currentMovie;
+    }
+
+    if (currentRating < lowestRating)
+    {
+        lowestRating = currentRating;
+        lowestRatingMovie = currentMovie;
+    }
+}
+
+Console.WriteLine($"{highestRatingMovie} is with highest rating: {highestRating:f1}");
+Console.WriteLine($"{lowestRatingMovie} is with lowest rating: {lowestRating:f1}");
+Console.WriteLine($"Average rating: {totalRating / moviesCount:f1}");
